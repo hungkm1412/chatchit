de2249889b5729c8a9af275de43d9367411e56e9
hungkm <hungkm@hungkm-H61M-D2P-B3.(none)>
2014-07-01 11:20:14 +0700

    Initialize devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatchit::Application.config.secret_token = '87584b0f9b2fe5df260025678db8d22808c9881e0660cf61dec80fcfc3943920cc815d136ee89ffa253d3577bbc39d6061f3c9d5f0cf76c740e113c19f16db36'
